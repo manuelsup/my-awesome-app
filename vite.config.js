65442a36e09da840e140de7e180eb8d089f77c8c
manuelarambula <162268917+manuelsup@users.noreply.github.com>
2026-05-15 00:00:24 -0700

    yes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-awsome-app/',
+  base: '/my-awesome-app/',
   plugins: [react()],
 })

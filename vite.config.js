7a01e721ee737e14b9570400794fcf97082c1934
manuelarambula <162268917+manuelsup@users.noreply.github.com>
2026-05-14 23:50:49 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-awsome-app/'
+  base: '/my-awsome-app/',
   plugins: [react()],
 })
